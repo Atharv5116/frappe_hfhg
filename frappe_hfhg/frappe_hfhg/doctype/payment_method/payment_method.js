a59b0b5f10439ecddb08cd513c74f50bd5849624
Atharv5116 <patelatharv5116@gmail.com>
2026-02-06 21:01:29 +0530

    Dynamic fields

---
+++ frappe_hfhg/frappe_hfhg/doctype/payment_method/payment_method.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, redsoft and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Payment method", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ frappe_hfhg/frappe_hfhg/doctype/payment_method/payment_method.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, redsoft and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Paymentmethod(Document):
+	pass
